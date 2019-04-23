1f6ef09cc57baa1f6f981ec60baa77fbd783bf0b
Ruben <42656904+6b726d@users.noreply.github.com>
2019-04-22 21:49:32 -0500

    Create functions.hpp

---
+++ TSP/functions.hpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+void DeleteElementPlus(vector<vector<int>> &table, vector<int> &sizes, int e);
+
+void DeleteElement(vector<vector<int>> &table, int e);
+
+void PrintIVector1D(vector<int> vec);
+
+void PrintIVector2D(vector<vector<int>> vec);
+
+void PrintDVector1D(vector<double> vec);
+
+void PrintDVector2D(vector<vector<double>> vec);
+
+double EDistance(double x1, double x2, double y1, double y2);
